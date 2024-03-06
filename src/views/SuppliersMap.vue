1dbbedf95517ebb3dab39739f1a900b240227557
nathalie <nathalie.soto@le-campus-numerique.fr>
2024-03-06 22:28:36 +0100

    map ok

--- src/views/SuppliersMap.vue
+++ src/views/SuppliersMap.vue
@@ -1,14 +1,43 @@
 <script setup>
-
 //  [45.1667, 5.7167];
+import "leaflet/dist/leaflet.css";
+import { ref } from 'vue';
+import { LMap, LTileLayer, LMarker, LPopup } from "@vue-leaflet/vue-leaflet";
 
+const zoom = ref(2);
+
+const suppliers = ref([
+  {
+    id: 1,
+    latitude: 45.171112,
+    longitude:  5.695952
+  },
+  {
+    id: 2,
+    latitude: 45.183152,
+    longitude: 5.699386
+  }
+]);
 
 </script>
 
 <template>
   <div>
     <h1>Carte des fournisseurs</h1>
-    <div ref="map" class="leaflet-map"></div>
+    <div style="height:600px; width:800px">
+      <l-map ref="map" v-model:zoom="zoom" :center="[47.41322, -1.219482]">
+        <l-tile-layer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            layer-type="base"
+            name="OpenStreetMap"
+        ></l-tile-layer>
+
+        <!-- Agregar marcadores desde la lista de proveedores -->
+        <l-marker v-for="supplier in suppliers" :key="supplier.id" :lat-lng="[supplier.latitude, supplier.longitude]">
+          <l-popup>{{ `Proveedor ID: ${supplier.id}` }}</l-popup>
+        </l-marker>
+      </l-map>
+    </div>
   </div>
 </template>
 
